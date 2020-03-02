91df6e1c3c92ebbe18dab7f18e9515b62f9e2f4e
Aldo Caruso <aldocaruso1991@gmail.com>
2020-03-02 17:43:21 +0100

    update

--- range.js
+++ range.js
@@ -1,3 +1,10 @@
 /**
 * Scrivere qui il codice dell'esercizio 1 del capitolo 4 di eloquentJS
 **/
+const range = (start, stop, step) =>(
+    Array.from({length: (stop - start)/ step +1}, (_, i) => start + (i * step))
+)
+
+console.log(range( 0, 4, 1))
+
+console.log(range( 0, 4, 1).reduce((a, b) => a + b, 0))
