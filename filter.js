de50e327e9739d2d0656ebb0a6bf845ad4c8e6a9
Aldo Caruso <aldocaruso1991@gmail.com>
2020-03-03 16:22:20 +0100

    devo fixare

--- filter.js
+++ filter.js
@@ -1,1 +1,16 @@
 
+let arrayProva = [1, 2, 3, 4, 5]
+
+
+
+
+let filterBase = function() {
+    let accum = []
+    for (let i = 0; i < arrayProva.length; i ++){
+        if (filterBase(arrayProva) < 3) {
+        accum = [...accum, i]
+        } 
+    }
+        
+return console.log(accum)
+}
