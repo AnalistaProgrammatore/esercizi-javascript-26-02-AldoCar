e5ad0316ed4cf1d729c4580e29bcd3075fcc6ecc
Aldo Caruso <aldocaruso1991@gmail.com>
2020-03-03 17:39:30 +0100

    update

--- reduce.js
+++ reduce.js
@@ -1,1 +1,8 @@
-
+let arrayProva = [1, 2, 3, 4, 5]
+let acc = []
+let reduceBase = function(arrayProva){
+    for (i = 0; i < arrayProva.length; i++){
+        return acc.concat(arrayProva)
+    }
+    return acc
+}
